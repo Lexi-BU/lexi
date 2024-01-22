627251aa25e48806dc4ecfe3268237fcc40a3db9
qudsiramiz <qudsiramiz@gmail.com>
2024-01-22 14:38:06 -0500

    exposure map

---
+++ lexi/lxi_non_lin_corr_20240122.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+# Define the row number for each row
+row_num = np.array([4, 3, 2, 1, 0, -1, -2, -3, -4])
+
+# Define the number of holes in each of the nine rows
+num_holes = np.array([1, 5, 7, 7, 9, 7, 7, 5, 1])
+
+# Define the distance between adjacent holes in each of the nine rows
+dist_h_inch = 0.394
+dist_h_cm = dist_h_inch * 2.54
+
+# Define the distance from x-axis to the center of the first hole in each of the nine rows
+dist_x_cm = row_num * dist_h_cm
+
+# Define the diameter of the holes
+d_h_inch = 0.020
+d_h_cm = d_h_inch * 2.54
+
+# Define the x and y coordinates of each of the holes
+x = np.array([])
+y = np.array([])
+for i in range(0, len(row_num)):
+    hole_number = np.arange(-(num_holes[i] - 1) / 2, (num_holes[i] - 1) / 2 + 1, 1)
+    y = np.append(y, dist_x_cm[i] * np.ones(len(hole_number)))
+    x = np.append(x, hole_number * dist_h_cm)
+
+# Add the location of four more holes separately
+x = np.append(x, np.array([-0.197, 0.197, 0.984, -0.197]) * 2.54)
+y = np.append(y, np.array([-0.591, -0.197, 0.197, 1.378]) * 2.54)
+xy = np.array([x, y])
+
+# Define a new coordinate system where the previous coordinate system is rotated by 45 degrees
+theta = np.radians(-50)
+c, s = np.cos(theta), np.sin(theta)
+R = np.array(((c, -s), (s, c)))
+xy_new = np.dot(R, xy)
+
+
+# Plot the coordinates of the holes in the new coordinate system
+plt.figure()
+plt.scatter(xy_new[0], xy_new[1], s=5, color="r")
+plt.xlabel("x (cm)")
+plt.ylabel("y (cm)")
+plt.title("Coordinates of the holes in the new coordinate system")
+# Add a circle to the plot of radius 4.5 cm
+circle = plt.Circle((0, 0), 4, color="r", fill=False)
+plt.gca().add_artist(circle)
+plt.axis("equal")
+
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+plt.savefig("../figures/coordinates.png", dpi=300)
+
+
+# Define the pickle file name
+pickle_file = (
+    "../data/2022_04_28_1313_LEXI_Sci_unit1_mcp_unit1_eBox_1900V_counts.pickle"
+)
+
+# Open the pickle file
+with open(pickle_file, "rb") as f:
+    counts = pickle.load(f)
+    xedges = pickle.load(f)
+    yedges = pickle.load(f)
+
+# Close the pickle file
+f.close()
+
+# Define the x and y coordinates of the center of each pixel
+x = np.array([])
+y = np.array([])
+for i in range(0, len(xedges) - 1):
+    x = np.append(x, (xedges[i] + xedges[i + 1]) / 2)
+    y = np.append(y, (yedges[i] + yedges[i + 1]) / 2)
+
+
+# At each xy_new coordinate, find the location of point with maximum count value within a circle of
+# radius 0.1 cm
+max_count = np.array([])
+plt.figure()
+for i in range(0, len(xy_new[0])):
+    dist = np.sqrt((x - xy_new[0][i]) ** 2 + (y - xy_new[1][i]) ** 2)
+    mask = dist <= 0.1
+    max_count = np.append(max_count, np.max(counts[mask]))
+    # Get the corresponding values of x and y
+    x_max = x[mask][np.argmax(counts[mask])]
+    y_max = y[mask][np.argmax(counts[mask])]
+    # Plot the location of the point with maximum count value
+    plt.scatter(x_max, y_max, s=5, color="r")
+
+plt.savefig("../figures/maximum_count.png", dpi=300)
