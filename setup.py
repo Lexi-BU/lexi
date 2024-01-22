c3145dd8daac64393ce304f498e2cb8d0c7a82c5
qudsiramiz <qudsiramiz@gmail.com>
2024-01-22 10:30:18 -0500

    added csv sample files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lexi",
+    version='0.1',
+    description="Data analysis tools for the Lexi project",
+    long_description=="open('README.md').read()",
+    url="https://github.com/Lexi-BU/lexi",
+    author='Lexi',
+    author_email="lunar.lexi01@gmail.com",
+    license="MIT",
+    keywords="data analysis",
+    packages=find_packages(),
+    install_requires=["spacepy"],
