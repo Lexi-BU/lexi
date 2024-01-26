931dca011c2e66d9a9508b63dabc808f6ba4283d
Zoe <19900057+vpsx@users.noreply.github.com>
2024-01-26 10:22:49 -0500

    Do preliminary clean-up of old docstrings

--- lexi/lxi_code_plan.py
+++ lexi/lxi_code_plan.py
@@ -10,6 +10,9 @@ from .lxi_exposure_map_fnc import exposure_map
 class LEXI:
 
     def __init__(self, input_params):
+        """
+        TODO all those comments below should probably just go in a nice docstring...
+        """
 
         self.LEXI_FOV = 9.1 # LEXI field of view in degrees
 
@@ -35,7 +38,15 @@ class LEXI:
         # ZLC: should document what time formats are appropriate
         # ZLC: should check this is not None and is [a, b]; maybe change them to pd.Timestamps right here
         # and raise error if cannot
+        #    --time: [start time, end time]
+        #    This will be a list of two elements, the first element is the start time and the second
+        #    element is the end time. Each element of the list can be either of the following types:
+        #    1. A string in the format 'YYYY-MM-DDTHH:MM:SS' (e.g. '2022-01-01T00:00:00')
+        #    2. A datetime object
+        #    3. A float in the format of a UNIX timestamp (e.g. 1640995200.0) **ZLC no, this needs more inspection/a unit
         self.t_range = input_params.get("t_range")
+
+
         # exposure map step time in seconds: Ephemeris data will be resampled and interpolated to this
         # time resolution; then, for each look-direction datum,
         # this many seconds are added to each in-FOV cell in the exposure map.
@@ -44,49 +55,33 @@ class LEXI:
         # integration time in seconds for lexi histograms and exposure maps
         self.t_integrate = input_params.get("t_integrate", 60)
 
-        self.ra_range = input_params.get("ra_range", [325.0, 365.0]) # RA range in degrees for plotted histogram
-        self.dec_range = input_params.get("dec_range", [-21.0, 6.0]) # DEC range in degrees for plotted histogram
-        self.ra_res = input_params.get("ra_res", 0.1) # RA res in degrees. Ideal value is 0.1 deg
-        self.dec_res = input_params.get("dec_res", 0.1) # DEC res in degrees. Ideal value is 0.1 deg
+
+        # RA range to plot, in degrees. [start RA, end RA]
+        self.ra_range = input_params.get("ra_range", [325.0, 365.0])
+        # DEC range to plot, in degrees. [start DEC, end DEC]
+        self.dec_range = input_params.get("dec_range", [-21.0, 6.0])
+        # RA resolution to plot at, in degrees. Ideal value is 0.1 deg.
+        self.ra_res = input_params.get("ra_res", 0.1)
+        # DEC resolution to plot at, in degrees. Ideal value is 0.1 deg.
+        self.dec_res = input_params.get("dec_res", 0.1)
+        # Alternative to ra_res/dec_res: nbins defines the number of bins in the RA and DEC directions.
+        # Either a scalar integer or [ra_nbins, dec_nbins].
         if input_params.get("nbins") is not None:
             nbins = input_params["nbins"]
+            ra_nbins, dec_nbins = (nbins[0],nbins[1]) if type(nbins) in [tuple, list] else (nbins, nbins)
             if input_params.get("ra_res") or input_params.get("dec_res"):
-                print(f"Requested both (ra_res and/or dec_res) and nbins; ignoring res value and using "
-                      f"requested nbins of {nbins}")
-            self.ra_res = (self.ra_range[1] - self.ra_range[0]) / nbins
-            self.dec_res = (self.dec_range[1] - self.dec_range[0]) / nbins
+                print(f"Both (ra_res and/or dec_res) and nbins were specified; ignoring res value and setting "
+                      f"RA nbins: {ra_nbins}, DEC nbins: {dec_nbins}")
+            self.ra_res = (self.ra_range[1] - self.ra_range[0]) / ra_nbins
+            self.dec_res = (self.dec_range[1] - self.dec_range[0]) / dec_nbins
 
 
-    # Define the first function called "get_spc_prams" that takes time as an argument and returns the
-    # time, look direction, and roll angle of the lander
     def get_spc_prams(self):
-        # Define the time, look direction (gamma) and the roll angle (phi) of the lander
-        # Define the format of each of the inputs
-        #    --time: [start time, end time]
-        #    This will be a list of two elements, the first element is the start time and the second
-        #    element is the end time. Each element of the list can be either of the following types:
-        #    1. A string in the format 'YYYY-MM-DDTHH:MM:SS' (e.g. '2022-01-01T00:00:00')
-        #    2. A datetime object
-        #    3. A float in the format of a UNIX timestamp (e.g. 1640995200.0)
-
-        #    --time_resolution: float
-        #    This will be a float that defines the time resolution of the data in seconds
-
-        #    --gamma: 1-d array of floats in the range of [0, 2*pi] where the length of the array
-        #    will be defined by the number of data points in the time range as derived from reading
-        #    the lander ephemeris file
-
-        #    --alpha: 1-d array of floats in the range of [0, 2*pi] where the length of the array
-        #    will be defined by the number of data points in the time range as derived from reading
-        #    the lander ephemeris file
-
-        #    --time_array: 1-d array of floats in the range of [time[0], time[1]] where the length of
-        #    the array will be defined by the number of data points in the time range as derived from
-        #    reading the lander ephemeris file
-
-        # If a time resolution is given then the code will also interpolate the ephemeris data to the
-        # given time resolution
-
+        """
+        Returns a dataframe containing all ephemeris data (time, look direction RA, look direction DEC,
+        roll angle, and potentially other columns), sliced to t_range and interpolated to t_step using
+        interp_method.
+        """
         # Note: Current sample data does not include roll angle.
         # Roll angle = gimbal + lander + altitude
 
@@ -102,43 +97,13 @@ class LEXI:
 
 
 
-    # Define a second function which takes the following list of arguments:
-    #    --time
-    #    --RA
-    #    --DEC
-    #    --binsize
-    #    --nbins
-    #    --integration_time
-    # The function then computes the sky background and returns the sky background
     def get_sky_background(self):
-        # Define the format of each of the inputs
-        #    --time: [start time, end time] (see above for the format of the time input)
-        #    --RA: [start RA, end RA]
-        #    This will be a list of two elements, the first element is the start RA and the second
-        #    element is the end RA. Each element of the list can be either of the following types:
-        #    1. A string in the format 'HH:MM:SS' (e.g. '00:00:00')
-        #    2. A float in the format of decimal hours (e.g. 0.0)
-
-        #    --DEC: [start DEC, end DEC]
-        #    This will be a list of two elements, the first element is the start DEC and the second
-        #    element is the end DEC. Each element of the list can be either of the following types:
-        #    1. A float in the format of decimal degrees (e.g. 0.0)
-
-        #    --binsize: [RA binsize, DEC binsize] (optional)
-        #    This will be a float that defines the size of the bins in degrees for DEC and in minutes
-        #    for RA.
-
-        #    --nbins: int or 1x2 array (optional)
-        #    This will be an integer that defines the number of bins in the RA and DEC directions
-
-        #    --integration_time: float
-        #    This will be a float that defines the integration time of the data in seconds
-
-        # The code will then compute the sky background for the given time, RA, DEC, binsize, nbins,
-        # and integration time and by accessing the get_spc_prams function to get the time, look
-        # direction (gamma) and the roll angle (phi) of the lander. The code will then return the
-        # sky background
-
+        """
+        Returns an array of ROSAT sky background images, corrected for LEXI exposure time.
+        Shape: num-images.ra-pixels.dec-pixels, where num-images depends on t_range and
+        t_integrate, ra-pixels depends on ra_range and ra_res, and dec-pixels depends on
+        dec_range and dec_res.
+        """
         # Get exposure maps
         exposure_maps = exposure_map(
                 spc_df = self.get_spc_prams(),
@@ -177,29 +142,12 @@ class LEXI:
 
         return sky_backgrounds
 
-    # Define a third function which takes the following list of arguments:
-    #    --time
-    #    --RA
-    #    --DEC
-    #    --binsize
-    #    --nbins
-    #    --integration_time
-    #    --background_correction
-    # The function then makes the background corrected image from LEXI data and returns the
-    # background corrected image
     def get_background_corrected_image(self):
-        # Define the format of each of the inputs
-        #    --time: [start time, end time] (see above for the format of the time input)
-        #    --RA: [start RA, end RA] (see above for the format of the RA input)
-        #    --DEC: [start DEC, end DEC] (see above for the format of the DEC input)
-        #    --binsize: [RA binsize, DEC binsize] (optional) (see above for the format of the binsize
-        #    input)
-        #    --nbins: int or 1x2 array (optional) (see above for the format of the nbins input)
-        #    --integration_time: float (see above for the format of the integration_time input)
-        #    --background_correction: bool
-        #    This will be a boolean that defines whether or not the background should be corrected
-        #    for the image. If True, the background will be corrected.
-
+        """
+        Returns an array of LEXI science histograms. Shape: num-images.ra-pixels.dec-pixels,
+        where num-images depends on t_range and t_integrate, ra-pixels depends on ra_range and
+        ra_res, and dec-pixels depends on dec_range and dec_res.
+        """
         # TODO: Get actual timeseries data
         # With ra/dec resolution of 3, and no bg correction, this will draw a smiley face;
         # bg correction lops off the left side of the smile
