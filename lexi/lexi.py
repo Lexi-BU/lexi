43f3bb007eaebdf632f566824496d021de4b7c0b
Ramiz Qudsi <qudsiramiz@gmail.com>
2024-12-13 19:01:06 -0500

    formatting edit

--- lexi/lexi.py
+++ lexi/lexi.py
@@ -252,44 +252,6 @@ def download_files_from_github(
     ValueError
         If the status code of the response is not 200
     """
-    """
-    Function to download files from a GitHub repository. Eventually, this function will be removed
-    and we will be able to use the `get_lexi_data` function to download the files directly from the
-    CDAweb website. For now, we will use this function to download the files from the GitHub to be
-    used as a placeholder until we have the real data hosted on the appropriate website.
-
-    NOTE: In this function, we are using two folders to store and download the files. The first
-    folder contains the first 950 files, and the second folder contains the remaining files. The
-    reason for this is that the GitHub API only returns a maximum of 1000 files per request. If the
-    folder contains more than 1000 files, then the files are split into multiple folders. The folder
-    names are as follows: files_0_to_950, files_950_to_1917. The folder names are hardcoded in the
-    function.
-
-    Parameters
-    ----------
-    file_name_list : list
-        List of file names to download
-    repo : str
-        Name of the GitHub repository
-    folder_path : str
-        Path to the folder in the GitHub repository
-    branch : str, optional
-        Name of the branch in the GitHub repository. Default is "main"
-    save_dir : str, optional
-        Directory to save the downloaded files. Default is "downloaded_data"
-    verbose : bool, optional
-        If True, print messages. Default is False
-
-    Returns
-    -------
-    local_file_list : list
-        List of local file paths
-
-    Raises
-    ------
-    ValueError
-        If the status code of the response is not 200
-    """
     # GitHub API URL for the folder
     # NOTE: The GitHub API only returns a maximum of 1000 files per request. If the folder contains
     # more than 1000 files, then the files are split into multiple folders. The first folder contains
@@ -306,7 +268,7 @@ def download_files_from_github(
         print(
             f"Error: Unable to access {api_url} (Status code: {response_1.status_code})"
         )
-        return
+        # return
     if response_2.status_code != 200:
         print(
             f"Error: Unable to access {api_url} (Status code: {response_2.status_code})"
@@ -374,14 +336,16 @@ def get_lexi_data(
     ----------
     time_range : list
         Time range to consider. [start time, end time]. Times can be expressed in the following
-        formats:
+    formats:
             1. A string in the format 'YYYY-MM-DDTHH:MM:SS' (e.g. '2022-01-01T00:00:00')
             2. A datetime object
             3. A float in the format of a UNIX timestamp (e.g. 1640995200.0)
-            This time range defines the time range of the ephemeris data and the time range of
-            the LEXI data.
-        Note that endpoints are inclusive (the end time is a closed interval); this is because
-        the time range slicing is done with pandas, and label slicing in pandas is inclusive.
+
+    This time range defines the time range of the ephemeris data and the time range of the LEXI data.
+
+    Note that endpoints are inclusive (the end time is a closed interval); this is because
+    the time range slicing is done with pandas, and label slicing in pandas is inclusive.
+
     time_zone : str, optional
         The timezone of the time range of interest. Default is "UTC"
     verbose : bool, optional
@@ -391,6 +355,7 @@ def get_lexi_data(
     -------
     df : pandas DataFrame
         LEXI data in a pandas DataFrame
+
     """
 
     # Validate time_range
@@ -1232,7 +1197,7 @@ def get_sky_backgrounds(
 
     force_compute : bool, optional
         If True, force the computation of the sky backgrounds even if a skybackground data is present
-       in the default folder. Default is False.
+        in the default folder. Default is False.
 
     Returns
     -------
