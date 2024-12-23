a2c4d48ea50673f570a4422395990df760d461b4
Ramiz Qudsi <qudsiramiz@gmail.com>
2024-12-23 11:41:43 -0500

    minor edit to __init__

--- lexi/__init__.py
+++ lexi/__init__.py
@@ -8,15 +8,15 @@ __doc__ = """
 The lexi is a package developed using the Python programming language. The package is
 designed to provide a simple list of functions to work with the LEXI dataset. The package has
 following usable modules:
-    - get_lexi_data: This module is used to get the LEXI dataset from the CDAweb website using a
+    - **get_lexi_data**: This module is used to get the LEXI dataset from the CDAweb website using a
       specified time range.
-    - get_spc_prams: This module is used to get the spacecraft parameters from the LEXI dataset using
+    - **get_spc_prams**: This module is used to get the spacecraft parameters from the LEXI dataset using
       a specified time range.
-    - get_exposure_maps: This module is used to get the exposure maps from the LEXI dataset using a
+    - **get_exposure_maps**: This module is used to get the exposure maps from the LEXI dataset using a
       specified time range and some other input parameters.
-    - get_sky_backgrounds: This module is used to get the sky backgrounds from the LEXI dataset which
+    - **get_sky_backgrounds**: This module is used to get the sky backgrounds from the LEXI dataset which
       corresponds to the exposure maps. The module uses the exposure maps to get the sky backgrounds.
-    - get_lexi_images: This module is used to get the LEXI images from the LEXI dataset using a
+    - **get_lexi_images**: This module is used to get the LEXI images from the LEXI dataset using a
       specified time range and some other input parameters. The module uses the exposure maps and sky
       backgrounds to get the LEXI images. One can either get a background corrected image or a raw
       image from the data set.
