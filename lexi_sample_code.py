3eff92146d9be1b55360de38497ae65ab50fdd26
qudsiramiz <qudsiramiz@gmail.com>
2024-12-24 16:42:58 -0500

    implemented the requested feature or array_to_image function

--- lexi_sample_code.py
+++ lexi_sample_code.py
@@ -4,42 +4,110 @@ import numpy as np
 
 importlib.reload(lexi)
 
+array_to_image_kwargs_exp = {
+    "x_range": [190, 240],
+    "y_range": [-30, -10],
+    "x_lim": [220, 240],
+    "y_lim": [-30, -15],
+    "cmap": "plasma",
+    "cmin": 1,
+    "norm": None,
+    "norm_type": "log",
+    "aspect": "equal",
+    "figure_title": "LEXI Exposure Map",
+    "show_colorbar": True,
+    "cbar_label": "Seconds",
+    "cbar_orientation": "vertical",
+    "show_axes": True,
+    "display": False,
+    "figure_size": (5, 5),
+    "figure_format": "png",
+    "figure_font_size": 12,
+    "save": True,
+    "save_name": "default",
+    "save_path": None,
+    "dpi": 300,
+    "dark_mode": True,
+    "verbose": True,
+}
+
+array_to_image_kwargs_sbg = {
+    "x_range": [190, 240],
+    "y_range": [-30, -10],
+    "x_lim": [220, 240],
+    "y_lim": [-30, -15],
+    "cmap": "plasma",
+    "cmin": 1,
+    "norm": None,
+    "norm_type": "log",
+    "aspect": "equal",
+    "figure_title": "Sky Backgrounds",
+    "show_colorbar": True,
+    "cbar_label": "Counts",
+    "cbar_orientation": "vertical",
+    "show_axes": True,
+    "display": False,
+    "figure_size": (5, 5),
+    "figure_format": "png",
+    "figure_font_size": 12,
+    "save": True,
+    "save_name": "default",
+    "save_path": None,
+    "dpi": 300,
+    "dark_mode": True,
+    "verbose": True,
+}
+
+array_to_image_kwargs_lex = {
+    "x_range": [190, 240],
+    "y_range": [-30, -10],
+    "x_lim": [220, 240],
+    "y_lim": [-30, -15],
+    "cmap": "plasma",
+    "cmin": 1,
+    "norm": None,
+    "norm_type": "log",
+    "aspect": "equal",
+    # "figure_title": "LEXI Image",
+    "show_colorbar": True,
+    "cbar_label": "Counts",
+    "cbar_orientation": "vertical",
+    "show_axes": True,
+    "display": False,
+    "figure_size": (5, 5),
+    "figure_format": "png",
+    "figure_font_size": 12,
+    "save": True,
+    "save_name": "default",
+    "save_path": None,
+    "dpi": 300,
+    "dark_mode": True,
+    "verbose": True,
+}
+
 input_params = {
     "time_range": ["2025-03-04 08:53:41", "2025-03-04 09:23:41"],
-    # "time_zone": "UTC",
-    # "time_integrate": 120,
-    # "time_step": "5",
-    # "ra_res": 1,
-    # "dec_res": 1,
-    # "ra_range": [190, 240],
-    # "dec_range": [-30, -10],
-    # "interp_method": "linear",
-    # save_df=True,
-    # filename="test_data/LEXI_pointing_ephem_highres",
-    # filetype="pkl",
-    # "save_exposure_map_file": True,
-    # "save_exposure_map_image": True,
-    # "save_sky_backgrounds_file": True,
-    # "save_sky_backgrounds_image": True,
-    # "background_correction_on": False,
-    # "save_lexi_images": True,
+    "ra_res": 1,
+    "dec_res": 1,
+    "ra_range": [190, 240],
+    "dec_range": [-30, -10],
+    "save_exposure_map_file": True,
+    "save_exposure_map_image": False,
+    "save_sky_backgrounds_file": True,
+    "save_sky_backgrounds_image": False,
+    "save_lexi_images": True,
     "verbose": True,
-    # "spc_prams": True,
-    "lexi_data": True,
-    "return_data_type": "all",
-    "time_pad": 900,
-    "data_clip": True,
-    # "spc_prams_kwargs": {"time_step": "60", "interp_method": "index"},
-    # "lexi_data_kwargs": {
-    # "force_compute": False,
+    "background_correction_on": True,
+    "array_to_image_kwargs": array_to_image_kwargs_lex,
 }
+
 
 # df1, df2, df3 = lexi.get_lexi_data(**input_params)
 # df3 = lexi.get_spc_prams(**input_params)
-df1, df2, df3 = lexi.get_spc_prams(**input_params)
-print(df1, df2, df3)
-print(input_params["time_range"])
-# exposure_maps_dict = lexi.get_exposure_maps(**input_params)
+# df1, df2, df3 = lexi.get_spc_prams(**input_params)
+# print(df1, df2, df3)
+# print(input_params["time_range"])
+exposure_maps_dict = lexi.get_lexi_images(**input_params)
 
 # exposure_maps = exposure_maps_dict["exposure_maps"]
 
