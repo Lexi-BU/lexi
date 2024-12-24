4a29e547afcd687ad632b21b6509a77ba9f0840e
qudsiramiz <qudsiramiz@gmail.com>
2024-12-24 13:03:58 -0500

    Update workflow documentation

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ html_context = {
     "github_user": "Lexi-BU",
     "github_repo": "lexi",
     "github_version": "stable",
-    "conf_py_path": "/docs/",
+    "conf_py_path": "./",
 }
 
 # -- Options for autodoc -----------------------------------------------------
