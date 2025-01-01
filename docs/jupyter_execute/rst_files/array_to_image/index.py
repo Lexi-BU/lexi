58c6c62f2495626bd7875dbd644597d06843876e
Ramiz Qudsi <qudsiramiz@gmail.com>
2025-01-01 09:52:03 -0500

    modified doctree from _build

---
+++ docs/jupyter_execute/rst_files/array_to_image/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from lexi_bu.lexi import array_to_image
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create a 2D array
+input_array = np.random.rand(100, 100)
+
+# Print the shape of the input array
+# The shape should be (100, 100)
+print(input_array.shape)
+
