c313a3374200a393312392e3a219b01fc70ef681
Ramiz Qudsi <qudsiramiz@gmail.com>
2025-01-03 16:59:25 -0500

    fixed conf_py_path issue

--- src/conf.py
+++ src/conf.py
@@ -83,7 +83,7 @@ html_context = {
     "github_user": "Lexi-BU",
     "github_repo": "lexi",
     "github_version": "stable",
-    "conf_py_path": "/src/rst_files/",
+    "conf_py_path": "/src/",
 }
 
 # -- Options for autodoc -----------------------------------------------------
